3a8337f7d64914b89e9d632bcee5a7312fd2d69f
Improwin <improwin@gmail.com>
2015-11-23 23:18:52 -0500

    completed email submission through Ajax

--- Improwin.Website/Controllers/HomeController.cs
+++ Improwin.Website/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using Newtonsoft.Json;
 using System.IO;
 using Newtonsoft.Json.Linq;
 using System.Text;
+using System.Collections;
 
 namespace Improwin.Website.Controllers
 {
@@ -27,57 +28,68 @@ namespace Improwin.Website.Controllers
         }
 
 
-        [HttpGet]
+        
+               
         public ActionResult Contact()
         {
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult Contact(Helper hl)
-        {
-
-            StringBuilder strMailBody = new StringBuilder();
-            strMailBody.Append("<html xmlns='http://www.w3.org/1999/xhtml'>");
-            strMailBody.Append("<head>");
-            strMailBody.Append("</head>");
-            strMailBody.Append("<body>");
-
-            #region MailBody
-            strMailBody.Append("<div><div><label><b>Name :</b></label>");
-            strMailBody.Append(hl.name);
-            strMailBody.Append("</div><div><label><b>Email:</b></label>");
-            strMailBody.Append(hl.email);
-            strMailBody.Append("</div><div><label><b>Phone :</b></label>");
-            strMailBody.Append(hl.ph);
-            strMailBody.Append("</div><div><label><b>Company Name :</b></label>");
-            strMailBody.Append(hl.company);
-            strMailBody.Append("</div><div><label><b>Subject:</b></label>");
-            strMailBody.Append(hl.subject);
-            strMailBody.Append("</div><div><label><b>Message:</b></label>");
-            strMailBody.Append(hl.msg);
-            strMailBody.Append("</div></div>");
-            strMailBody.Append("</body>");
-            strMailBody.Append("</html>");
-
-            EmailHandler _emailHanlder = new EmailHandler();
-            _emailHanlder._subject = hl.subject;
-            _emailHanlder._sender = "info@improwinsolutions.com";
-            _emailHanlder._displayName = "Improwin Sender";
-            _emailHanlder._mailbody = strMailBody;
-            _emailHanlder._recipients = new string[] {"info@improwinsolutions.com"};
-
-            _emailHanlder.Send();
-
-            #endregion MailBody
+            if (Request.HttpMethod == "POST")
+            {
+                Hashtable hs = new Hashtable();
+                foreach (string item in Request.Form)
+                {
+                    hs.Add(item, Request.Form[item]);
+                }
 
 
-            //Main Table End
+                StringBuilder strMailBody = new StringBuilder();
+                strMailBody.Append("<html xmlns='http://www.w3.org/1999/xhtml'>");
+                strMailBody.Append("<head>");
+                strMailBody.Append("</head>");
+                strMailBody.Append("<body>");
+
+                #region MailBody
+                strMailBody.Append("<div><div><label><b>Name :</b></label>");
+                strMailBody.Append(hs["name"].ToString());
+                strMailBody.Append("</div><div><label><b>Email:</b></label>");
+                strMailBody.Append(hs["email"].ToString());
+                strMailBody.Append("</div><div><label><b>Phone :</b></label>");
+                strMailBody.Append(hs["ph"].ToString());
+                strMailBody.Append("</div><div><label><b>Company Name :</b></label>");
+                strMailBody.Append(hs["company"].ToString());
+                strMailBody.Append("</div><div><label><b>Subject:</b></label>");
+                strMailBody.Append(hs["subject"].ToString());
+                strMailBody.Append("</div><div><label><b>Message:</b></label>");
+                strMailBody.Append(hs["msg"].ToString());
+                strMailBody.Append("</div></div>");
+                strMailBody.Append("</body>");
+                strMailBody.Append("</html>");
+
+                EmailHandler _emailHanlder = new EmailHandler();
+                _emailHanlder._subject = hs["subject"].ToString();
+                _emailHanlder._sender = "info@improwinsolutions.com";
+                _emailHanlder._displayName = "Improwin Sender";
+                _emailHanlder._mailbody = strMailBody;
+                _emailHanlder._recipients = new string[] { "info@improwinsolutions.com" };
+
             
+                    if (_emailHanlder.Send() == true)
+                    { return this.Json(new { msg = "success" }); }
+                    else { return this.Json(new { msg = "failure" }); }
+
+                    #endregion MailBody
+
+
+                    //Main Table End
 
 
 
-            return this.Json(new { msg = "success" });
+
+                    
+
+                
+            }
+            else
+            { return View(); }
             
 
         }
